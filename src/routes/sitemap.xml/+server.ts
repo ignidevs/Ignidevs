460dbbb6257339379b74fd489095508a94e373d7
kloparn <adam97.live.com@live.com>
2026-05-14 14:30:55 +0200

    fix sitemap

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -8,21 +8,16 @@ export function GET() {
 
   const urls = LOCALES.map((lang) => {
     const loc = `${SITE.url}/${lang}/`;
-    const alternates = LOCALES.map(
-      (l) => `    <xhtml:link rel="alternate" hreflang="${l}" href="${SITE.url}/${l}/" />`
-    ).join('\n');
     return `  <url>
     <loc>${loc}</loc>
     <lastmod>${lastmod}</lastmod>
     <changefreq>monthly</changefreq>
     <priority>1.0</priority>
-${alternates}
-    <xhtml:link rel="alternate" hreflang="x-default" href="${SITE.url}/en/" />
   </url>`;
   }).join('\n');
 
   const body = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap-0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 ${urls}
 </urlset>
 `;
